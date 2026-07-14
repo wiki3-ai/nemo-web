990381b62c74ed13ee6c8d980855751fcf0f06cd
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2026-07-14 23:43:27 +0200

    Add N3 example

--- src/components/examples/files/listOfExamples.ts
+++ src/components/examples/files/listOfExamples.ts
@@ -1,13 +1,14 @@
-import elCalcCSVURL from "./elCalcCSV.rls?url";
-import elCalcOWLRDFURL from "./elCalcOWLRDF.rls?url";
-import oldLimeTreesURL from "./oldLimeTrees.rls?url";
-import divByZeroURL from "./div-by-zero.rls?url";
-import findCommonAncestors from "./findCommonAncestors.rls?url";
-import findCommonAncestorsWikidata from "./findCommonAncestorsWikidata.rls?url";
-import wikipediaTitlesVsLabels from "./wikipediaTitlesVsLabels.rls?url";
-import longestWinningStreaks from "./longestWinningStreaks.rls?url";
-import carbonMajorCountries from "./carbonMajorCountries.rls?url";
-import emptyWikidataClasses from "./emptyWikidataClasses.rls?url";
+// import elCalcCSVURL from "./elCalcCSV.rls?url";
+// import elCalcOWLRDFURL from "./elCalcOWLRDF.rls?url";
+// import oldLimeTreesURL from "./oldLimeTrees.rls?url";
+// import divByZeroURL from "./div-by-zero.rls?url";
+//import findCommonAncestors from "./findCommonAncestors.rls?url";
+import findCommonAncestors from "./findCommonAncestors.n3?url";
+// import findCommonAncestorsWikidata from "./findCommonAncestorsWikidata.rls?url";
+// import wikipediaTitlesVsLabels from "./wikipediaTitlesVsLabels.rls?url";
+// import longestWinningStreaks from "./longestWinningStreaks.rls?url";
+// import carbonMajorCountries from "./carbonMajorCountries.rls?url";
+// import emptyWikidataClasses from "./emptyWikidataClasses.rls?url";
 
 export const listOfExamples: {
   name: string;
@@ -17,40 +18,40 @@ export const listOfExamples: {
     name: "Ancestors (basic)",
     url: findCommonAncestors,
   },
-  {
-    name: "Ancestors of Ada and Moby (Wikidata, SPARQL)",
-    url: findCommonAncestorsWikidata,
-  },
-  {
-    name: "Wikipedia articles vs. labels (Wikidata, RDF, string functions)",
-    url: wikipediaTitlesVsLabels,
-  },
-  {
-    name: "Old trees (CSV, Wikidata)",
-    url: oldLimeTreesURL,
-  },
-  {
-    name: "Winning streaks in sports (Wikidata, SPARQL, conditional path length)",
-    url: longestWinningStreaks,
-  },
-  {
-    name: "CO2 emitting countries (CSV+Wikidata integration, aggregation)",
-    url: carbonMajorCountries,
-  },
-  {
-    name: "Empty classes in Wikidata (Wikidata ontology analysis, SPARQL)",
-    url: emptyWikidataClasses,
-  },
-  {
-    name: "Ontology reasoning in OWL EL (from preprocessed ontologies)",
-    url: elCalcCSVURL,
-  },
-  {
-    name: "Ontology reasoning in OWL EL (from OWL/RDF)",
-    url: elCalcOWLRDFURL,
-  },
-  {
-    name: "Div by Zero Analysis (using pregenerated input files for a fixed Java Program)",
-    url: divByZeroURL,
-  },
+  // {
+  //   name: "Ancestors of Ada and Moby (Wikidata, SPARQL)",
+  //   url: findCommonAncestorsWikidata,
+  // },
+  // {
+  //   name: "Wikipedia articles vs. labels (Wikidata, RDF, string functions)",
+  //   url: wikipediaTitlesVsLabels,
+  // },
+  // {
+  //   name: "Old trees (CSV, Wikidata)",
+  //   url: oldLimeTreesURL,
+  // },
+  // {
+  //   name: "Winning streaks in sports (Wikidata, SPARQL, conditional path length)",
+  //   url: longestWinningStreaks,
+  // },
+  // {
+  //   name: "CO2 emitting countries (CSV+Wikidata integration, aggregation)",
+  //   url: carbonMajorCountries,
+  // },
+  // {
+  //   name: "Empty classes in Wikidata (Wikidata ontology analysis, SPARQL)",
+  //   url: emptyWikidataClasses,
+  // },
+  // {
+  //   name: "Ontology reasoning in OWL EL (from preprocessed ontologies)",
+  //   url: elCalcCSVURL,
+  // },
+  // {
+  //   name: "Ontology reasoning in OWL EL (from OWL/RDF)",
+  //   url: elCalcOWLRDFURL,
+  // },
+  // {
+  //   name: "Div by Zero Analysis (using pregenerated input files for a fixed Java Program)",
+  //   url: divByZeroURL,
+  // },
 ];
